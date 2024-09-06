5abc9b4c93ef3b6d22672a4d32d18a90b831c4f8
José Á. Torrens Hernández <pptrrns@gmail.com>
2024-09-05 21:48:07 -0600

    Tidy .bib files and format Python files

---
+++ .pre-commit-hooks.d/bib-tidy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Running pre-commit hook in directory: $(pwd)"
+
+for bib_file in $(find . -type f -name "*.bib"); do
+  if [ -f "$bib_file" ]; then
+    echo "Tidying $bib_file"
+    # Pasar el archivo como argumento directamente sin "--modify" ya que bibtex-tidy lo modifica automáticamente
+    bibtex-tidy --curly --numeric --months --space=4 --tab --strip-enclosing-braces --sort --drop-all-caps "$bib_file"
+  fi
+done
+
+echo "Pre-commit hook completed."
