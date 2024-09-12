54ad9fa7ce92fe6c0649639a80ff2983c1ec7795
José Ángel Torrens Hernández <jose.torrens@kof.com.mx>
2024-09-11 20:24:07 -0600

    Add pre-commit to remove LaTeX auxiliary files

---
+++ .pre-commit-hooks.d/rm-tex.sh
@@ -0,0 +1,1 @@
+find . -type f \( -name "*.xdv" -o -name "*.aux" -o -name "*.log" -o -name "*.lot" -o -name "*.out" -o -name "*.ilg" -o -name "*.idx" -o -name "*.ind" -o -name "*.toc" -o -name "*.bbl" -o -name "*.blg" -o -name "*.fdb_latexmk" -o -name "*.fls" -o -name "*.lof" -o -name "*.synctex(busy)" -o -name "*.synctex.gz" \) -delete
