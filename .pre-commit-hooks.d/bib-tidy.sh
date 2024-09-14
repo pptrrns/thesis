0b91f2030ec70c83b78182ebcb0e84b15410a375
José Ángel Torrens Hernández <pptrrns@gmail.com>
2024-09-14 10:36:53 -0600

    Run pre-commit hook

--- .pre-commit-hooks.d/bib-tidy.sh
+++ .pre-commit-hooks.d/bib-tidy.sh
@@ -6,7 +6,7 @@ for bib_file in $(find . -type f -name "*.bib"); do
   if [ -f "$bib_file" ]; then
     echo "Tidying $bib_file"
     # Pasar el archivo como argumento directamente sin "--modify" ya que bibtex-tidy lo modifica automáticamente
-    bibtex-tidy --curly --numeric --months --modify --space=4 --tab --strip-enclosing-braces --sort --drop-all-caps "$bib_file"
+    bibtex-tidy --curly --numeric --months --modify --space=4 --tab --strip-enclosing-braces --sort=key --duplicates=doi --omit=abstract,keywords --drop-all-caps --sort-fields=author,title,year --remove-empty-fields --escape --encode-urls references.bib
   fi
 done
 
